285133d03d5f2bea40e42e5ce85d846ba37a6cbe
JR Boroy <jmesrafael@gmail.com>
2026-05-04 14:46:55 +0800

    Allow localhost:3001 in CORS for dev server

--- sawo-main/backend/server.js
+++ sawo-main/backend/server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "https://sawogitsrc.vercel.app",
   "http://localhost:3000",
+  "http://localhost:3001",
   "http://localhost:5173"
 ];
 
